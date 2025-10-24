af548ff73e21df571d5b7dc22ef6a0689bed05f8
Rob Sessink <rob.sessink@yucca.io>
2025-10-24 19:48:27 +0200

    EXER: format

--- leap/leap.ml
+++ leap/leap.ml
@@ -1,4 +1,4 @@
-let leap_year year = 
-    if year mod 4 <> 0 then false
-    else if year mod 100 <> 0 then true
-    else (year mod 400 = 0)
+let leap_year year =
+  if year mod 4 <> 0 then false
+  else if year mod 100 <> 0 then true
+  else year mod 400 = 0
